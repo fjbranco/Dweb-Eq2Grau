d54eb2e9ccc3fe3340ba31e28d222f0b784c98e0
Filipe Branco <estt6967@ipt.pt>
2026-03-03 15:08:25 +0000

    Validação dos dados e aprimorar resultados da equação

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -7,10 +7,19 @@ namespace Eq2Grau.Controllers
 {
     public class HomeController : Controller
     {
+        // GET
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         /// <summary>
         /// 'porta' de entrada no programa
         /// </summary>
         /// <returns></returns>
+        // POST
+        [HttpPost]
         public IActionResult Index(string A, string B, string C)
         {
             /*
@@ -21,13 +30,45 @@ namespace Eq2Grau.Controllers
              *   mostrar resultado x1 e x2
              * senão, mostrar mensagem de erro
              */
-            //if(!float.TryParse(A, out float a)){}
-            float.TryParse(A, out float a);
-            float.TryParse(B, out float b);
-            float.TryParse(C, out float c);
 
+            /*float.TryParse(A, out float a);
+              float.TryParse(B, out float b);
+              float.TryParse(C, out float c);*/
+
+            A = A.Replace('.', ',');
+            B = B.Replace('.', ',');
+            C = C.Replace('.', ',');
+
+            if (!float.TryParse(A, out float a) ||
+               !float.TryParse(B, out float b) ||
+               !float.TryParse(C, out float c)) {
+                ViewBag.Erro = "Os valores A, B e C devem ser números válidos.";
+                return View();
+            }
+
+            // Validar se A é diferente de 0
+            if (a == 0)
+            {
+                ViewBag.Erro = "O valor de A deve ser diferente de 0.";
+                return View();
+            }
+
+            
             // Calcular raízes
             float delta = b * b - 4 * a * c;
+
+            // Verificar se delta tem raízes complexas
+            if (delta < 0)
+            {
+                // ViewBag.Erro = "A equação não possui raízes reais.";
+                double parteReal = -b / (2 * a);
+                double parteImaginaria = Math.Sqrt(-delta) / (2 * a);
+
+                ViewBag.X1 = parteReal + " + " + parteImaginaria + "i";
+                ViewBag.X2 = parteReal + " + " + parteImaginaria + "i";
+                return View();
+            }
+
             float x1 = (-b + MathF.Sqrt(delta)) / (2 * a);
             float x2 = (-b - MathF.Sqrt(delta)) / (2 * a);
 
